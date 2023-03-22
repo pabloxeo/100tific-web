172ec8c4a0b6a6938913647e4e0a073a09251e23
pabloxeoLP <pabloxeo@gmail.com>
2023-03-22 18:23:42 +0100

    fin p2

--- funciones.js
+++ funciones.js
@@ -14,6 +14,10 @@ function changeCSS(){
     }
 }
 
+/*
+Modificar el tamaño cuando abrimos la barra lateral
+*/ 
+
 function openNav() {
     if(document.getElementById("wrapper_pr").style.marginRight == "2%"){
         document.getElementById("mySidebar").style.width = "400px";
@@ -24,14 +28,39 @@ function openNav() {
     }
 }
 
+/*
+Ajuste al cerrar barra lateral
+*/
+
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("wrapper_pr").style.marginRight= "2%";
 }
 
-function comentar(){
-    document.getElementById("nombre_ej").innerHTML = document.getElementById("n").value;
-    document.getElementById("correo_ej").innerHTML = document.getElementById("e").value;
-    document.getElementById("texto_ej").innerHTML = document.getElementById("c").value; 
-}
+/*
+Funcion para enviar comentario
+*/
 
+function comentar(){
+    var re = new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+    if(document.getElementById("n").value == "" 
+    || document.getElementById("e").value == "" 
+    || document.getElementById("c").value == ""
+    ){
+    }else if(re.test(document.getElementById("e").value)){
+        document.getElementById("comment_ej").style.visibility = "visible";
+        document.getElementById("nombre_ej").innerHTML = document.getElementById("n").value;
+        document.getElementById("correo_ej").innerHTML = document.getElementById("e").value;    
+        document.getElementById("texto_ej").innerHTML = document.getElementById("c").value;
+        document.getElementById("fecha").innerHTML = (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':'); 
+    } 
+}
+/*
+Funcion de escucha para reemplazar palabras al escribir
+*/
+
+document.getElementById("c").addEventListener("keypress", function(){
+    var texto = document.getElementById("c").value; 
+    texto = texto.replace(/joder|niño|carrera|casa/gi, "*****");
+    document.getElementById("c").value = texto;
+});
